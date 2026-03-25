cc452034d89b7e15ecfd5375038c19c9d3005bfb
Vojtěch Janů <januv.08@spst.cz>
2026-03-25 22:09:12 +0100

    v0.2

--- ConvAll/Form1.Designer.cs
+++ ConvAll/Form1.Designer.cs
@@ -28,157 +28,257 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             comboBox1 = new ComboBox();
             pnlVaha = new Panel();
-            comboBox3 = new ComboBox();
-            comboBox2 = new ComboBox();
+            textVystupVah = new TextBox();
+            comVahVstup = new ComboBox();
+            comVahVystup = new ComboBox();
             label2 = new Label();
-            numericUpDown2 = new NumericUpDown();
-            numericUpDown1 = new NumericUpDown();
+            numVaha = new NumericUpDown();
             pnlVzdalenost = new Panel();
+            textVystupVzda = new TextBox();
+            numVzda = new NumericUpDown();
+            comVzdaVstup = new ComboBox();
+            comVzdaVystup = new ComboBox();
             label1 = new Label();
-            numericUpDown3 = new NumericUpDown();
-            numericUpDown4 = new NumericUpDown();
+            label3 = new Label();
+            label4 = new Label();
+            notifyIcon1 = new NotifyIcon(components);
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            ukoncitToolStripMenuItem = new ToolStripMenuItem();
             pnlVaha.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numVaha).BeginInit();
             pnlVzdalenost.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numVzda).BeginInit();
+            contextMenuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // comboBox1
             // 
+            comboBox1.Anchor = AnchorStyles.None;
             comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "Vzdálenost", "Váha" });
-            comboBox1.Location = new Point(332, 65);
+            comboBox1.Items.AddRange(new object[] { "Váha", "Vzdálenost" });
+            comboBox1.Location = new Point(153, 150);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(121, 23);
+            comboBox1.Size = new Size(100, 23);
             comboBox1.TabIndex = 0;
             comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
             // 
             // pnlVaha
             // 
-            pnlVaha.Controls.Add(comboBox3);
-            pnlVaha.Controls.Add(comboBox2);
+            pnlVaha.Anchor = AnchorStyles.None;
+            pnlVaha.BackColor = Color.SteelBlue;
+            pnlVaha.Controls.Add(textVystupVah);
+            pnlVaha.Controls.Add(comVahVstup);
+            pnlVaha.Controls.Add(comVahVystup);
             pnlVaha.Controls.Add(label2);
-            pnlVaha.Controls.Add(numericUpDown2);
-            pnlVaha.Controls.Add(numericUpDown1);
-            pnlVaha.Location = new Point(537, 205);
+            pnlVaha.Controls.Add(numVaha);
+            pnlVaha.Location = new Point(50, 306);
             pnlVaha.Name = "pnlVaha";
-            pnlVaha.Size = new Size(200, 102);
+            pnlVaha.Size = new Size(300, 100);
             pnlVaha.TabIndex = 1;
             pnlVaha.Visible = false;
             // 
-            // comboBox3
+            // textVystupVah
             // 
-            comboBox3.FormattingEnabled = true;
-            comboBox3.Items.AddRange(new object[] { "t", "kg", "g", "mg" });
-            comboBox3.Location = new Point(150, 63);
-            comboBox3.Name = "comboBox3";
-            comboBox3.Size = new Size(33, 23);
-            comboBox3.TabIndex = 4;
+            textVystupVah.Location = new Point(10, 60);
+            textVystupVah.Name = "textVystupVah";
+            textVystupVah.ReadOnly = true;
+            textVystupVah.Size = new Size(150, 23);
+            textVystupVah.TabIndex = 6;
             // 
-            // comboBox2
+            // comVahVstup
             // 
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Items.AddRange(new object[] { "t", "kg", "g", "mg" });
-            comboBox2.Location = new Point(150, 26);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(33, 23);
-            comboBox2.TabIndex = 3;
+            comVahVstup.FormattingEnabled = true;
+            comVahVstup.Items.AddRange(new object[] { "kilogram (kg)", "gram (g)", "libra (lb)", "tuna (t)", "dekagram (dag)", "miligram (mg)", "metrický cent (q)", "unce (oz)" });
+            comVahVstup.Location = new Point(180, 25);
+            comVahVstup.Name = "comVahVstup";
+            comVahVstup.Size = new Size(115, 23);
+            comVahVstup.TabIndex = 4;
+            comVahVstup.TextChanged += numVaha_ValueChanged;
+            // 
+            // comVahVystup
+            // 
+            comVahVystup.FormattingEnabled = true;
+            comVahVystup.Items.AddRange(new object[] { "kilogram (kg)", "gram (g)", "libra (lb)", "tuna (t)", "dekagram (dag)", "miligram (mg)", "metrický cent (q)", "unce (oz)" });
+            comVahVystup.Location = new Point(180, 60);
+            comVahVystup.Name = "comVahVystup";
+            comVahVystup.Size = new Size(115, 23);
+            comVahVystup.TabIndex = 3;
+            comVahVystup.TextChanged += numVaha_ValueChanged;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(25, 8);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(25, 7);
             label2.Name = "label2";
             label2.Size = new Size(32, 15);
             label2.TabIndex = 2;
             label2.Text = "Váha";
             // 
-            // numericUpDown2
+            // numVaha
             // 
-            numericUpDown2.Location = new Point(13, 64);
-            numericUpDown2.Name = "numericUpDown2";
-            numericUpDown2.Size = new Size(120, 23);
-            numericUpDown2.TabIndex = 1;
-            // 
-            // numericUpDown1
-            // 
-            numericUpDown1.Location = new Point(12, 26);
-            numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(120, 23);
-            numericUpDown1.TabIndex = 0;
+            numVaha.DecimalPlaces = 2;
+            numVaha.Increment = new decimal(new int[] { 1, 0, 0, 65536 });
+            numVaha.Location = new Point(10, 25);
+            numVaha.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            numVaha.Name = "numVaha";
+            numVaha.Size = new Size(150, 23);
+            numVaha.TabIndex = 0;
+            numVaha.ValueChanged += numVaha_ValueChanged;
             // 
             // pnlVzdalenost
             // 
+            pnlVzdalenost.Anchor = AnchorStyles.None;
+            pnlVzdalenost.BackColor = Color.SteelBlue;
+            pnlVzdalenost.Controls.Add(textVystupVzda);
+            pnlVzdalenost.Controls.Add(numVzda);
+            pnlVzdalenost.Controls.Add(comVzdaVstup);
+            pnlVzdalenost.Controls.Add(comVzdaVystup);
             pnlVzdalenost.Controls.Add(label1);
-            pnlVzdalenost.Controls.Add(numericUpDown3);
-            pnlVzdalenost.Controls.Add(numericUpDown4);
-            pnlVzdalenost.Location = new Point(320, 205);
+            pnlVzdalenost.Location = new Point(50, 200);
             pnlVzdalenost.Name = "pnlVzdalenost";
-            pnlVzdalenost.Size = new Size(200, 102);
+            pnlVzdalenost.Size = new Size(300, 100);
             pnlVzdalenost.TabIndex = 3;
             pnlVzdalenost.Visible = false;
+            // 
+            // textVystupVzda
+            // 
+            textVystupVzda.Location = new Point(10, 60);
+            textVystupVzda.Name = "textVystupVzda";
+            textVystupVzda.ReadOnly = true;
+            textVystupVzda.Size = new Size(150, 23);
+            textVystupVzda.TabIndex = 7;
+            // 
+            // numVzda
+            // 
+            numVzda.DecimalPlaces = 2;
+            numVzda.Increment = new decimal(new int[] { 1, 0, 0, 65536 });
+            numVzda.Location = new Point(10, 25);
+            numVzda.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            numVzda.Name = "numVzda";
+            numVzda.Size = new Size(150, 23);
+            numVzda.TabIndex = 7;
+            numVzda.ValueChanged += numVzda_ValueChanged;
+            // 
+            // comVzdaVstup
+            // 
+            comVzdaVstup.FormattingEnabled = true;
+            comVzdaVstup.Items.AddRange(new object[] { "metr (m)", "centimetr (cm)", "palec (in)", "milimetr (mm)", "stopa (ft)", "decimetr (dm)", "kilometr (km)", "míle (mi)", "yard (yd)" });
+            comVzdaVstup.Location = new Point(180, 25);
+            comVzdaVstup.Name = "comVzdaVstup";
+            comVzdaVstup.Size = new Size(115, 23);
+            comVzdaVstup.TabIndex = 7;
+            comVzdaVstup.TextChanged += numVzda_ValueChanged;
+            // 
+            // comVzdaVystup
+            // 
+            comVzdaVystup.FormattingEnabled = true;
+            comVzdaVystup.Items.AddRange(new object[] { "metr (m)", "centimetr (cm)", "palec (in)", "milimetr (mm)", "stopa (ft)", "decimetr (dm)", "kilometr (km)", "míle (mi)", "yard (yd)" });
+            comVzdaVystup.Location = new Point(180, 60);
+            comVzdaVystup.Name = "comVzdaVystup";
+            comVzdaVystup.Size = new Size(115, 23);
+            comVzdaVystup.TabIndex = 8;
+            comVzdaVystup.TextChanged += numVzda_ValueChanged;
             // 
             // label1
             // 
             label1.AutoSize = true;
+            label1.ForeColor = Color.White;
             label1.Location = new Point(13, 8);
             label1.Name = "label1";
             label1.Size = new Size(64, 15);
             label1.TabIndex = 2;
             label1.Text = "Vzdálenost";
             // 
-            // numericUpDown3
+            // label3
             // 
-            numericUpDown3.Location = new Point(13, 64);
-            numericUpDown3.Name = "numericUpDown3";
-            numericUpDown3.Size = new Size(120, 23);
-            numericUpDown3.TabIndex = 1;
+            label3.AutoSize = true;
+            label3.Font = new Font("Stencil", 38F, FontStyle.Bold | FontStyle.Underline);
+            label3.ForeColor = Color.SteelBlue;
+            label3.Location = new Point(75, 41);
+            label3.Name = "label3";
+            label3.Size = new Size(245, 60);
+            label3.TabIndex = 4;
+            label3.Text = "ConvAll";
             // 
-            // numericUpDown4
+            // label4
             // 
-            numericUpDown4.Location = new Point(12, 26);
-            numericUpDown4.Name = "numericUpDown4";
-            numericUpDown4.Size = new Size(120, 23);
-            numericUpDown4.TabIndex = 0;
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F);
+            label4.Location = new Point(153, 132);
+            label4.Name = "label4";
+            label4.Size = new Size(96, 15);
+            label4.TabIndex = 5;
+            label4.Text = "Vyberte moznost";
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
+            notifyIcon1.Text = "notifyIcon1";
+            notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { ukoncitToolStripMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(116, 26);
+            // 
+            // ukoncitToolStripMenuItem
+            // 
+            ukoncitToolStripMenuItem.Name = "ukoncitToolStripMenuItem";
+            ukoncitToolStripMenuItem.Size = new Size(115, 22);
+            ukoncitToolStripMenuItem.Text = "Ukoncit";
+            ukoncitToolStripMenuItem.Click += ukoncitToolStripMenuItem_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(384, 461);
+            Controls.Add(label4);
+            Controls.Add(label3);
             Controls.Add(pnlVaha);
             Controls.Add(comboBox1);
             Controls.Add(pnlVzdalenost);
             Name = "Form1";
             Text = "Form1";
+            FormClosing += Form1_FormClosing;
             Load += Form1_Load;
             pnlVaha.ResumeLayout(false);
             pnlVaha.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numVaha).EndInit();
             pnlVzdalenost.ResumeLayout(false);
             pnlVzdalenost.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numVzda).EndInit();
+            contextMenuStrip1.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private ComboBox comboBox1;
         private Panel pnlVaha;
-        private NumericUpDown numericUpDown2;
-        private NumericUpDown numericUpDown1;
+        private NumericUpDown numVaha;
         private Label label2;
         private Panel pnlVzdalenost;
         private Label label1;
-        private NumericUpDown numericUpDown3;
-        private NumericUpDown numericUpDown4;
-        private ComboBox comboBox3;
-        private ComboBox comboBox2;
+        private ComboBox comVahVstup;
+        private ComboBox comVahVystup;
+        private TextBox textVystupVah;
+        private TextBox textVystupVzda;
+        private NumericUpDown numVzda;
+        private ComboBox comVzdaVystup;
+        private ComboBox comVzdaVstup;
+        private Label label3;
+        private Label label4;
+        private NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem ukoncitToolStripMenuItem;
     }
 }
--- ConvAll/Form1.cs
+++ ConvAll/Form1.cs
@@ -1,7 +1,52 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace ConvAll
 {
     public partial class Form1 : Form
     {
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == 0x0312)
+            {
+                if (this.Visible)
+                {
+                    this.Hide();
+                }
+                else
+                {
+                    this.Show();
+                    this.WindowState = FormWindowState.Normal;
+                    this.Activate();
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true; // Zrušíme příkaz k vypnutí
+                this.Hide();     // Jen schováme okno
+            }
+        }
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        // Definice kláves (Modifikátory)
+        enum KeyModifier
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            WinKey = 8
+        }
         public Form1()
         {
             InitializeComponent();
@@ -16,19 +61,283 @@ namespace ConvAll
             if (comboBox1.Text == "Vzdálenost")
             {
                 pnlVzdalenost.Visible = true;
-
+                comVzdaVstup.SelectedIndex = 0;
+                comVzdaVystup.SelectedIndex = 1;
 
             }
             if (comboBox1.Text == "Váha")
             {
                 pnlVaha.Visible = true;
+                comVahVstup.SelectedIndex = 0;
+                comVahVystup.SelectedIndex = 1;
 
             }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            comboBox1.SelectedIndex = 0;
+            RegisterHotKey(this.Handle, 1, (int)KeyModifier.Control + (int)KeyModifier.Shift, 0x51);
+        }
 
+        private void numVaha_ValueChanged(object sender, EventArgs e)
+        {
+            double vstup = Convert.ToDouble(numVaha.Value);
+            double vystup = 0;
+            double gram = 0;
+
+            switch (comVahVstup.Text)
+            {
+
+                case "tuna (t)":
+                    {
+                        gram = vstup * Math.Pow(10, 6);
+                        break;
+                    }
+                case "kilogram (kg)":
+                    {
+                        gram = vstup * Math.Pow(10, 3);
+                        break;
+                    }
+                case "dekagram (dag)":
+                    {
+                        gram = vstup * Math.Pow(10, 1);
+                        break;
+                    }
+                case "gram (g)":
+                    {
+                        gram = vstup;
+                        break;
+                    }
+                case "miligram (mg)":
+                    {
+                        gram = vstup * Math.Pow(10, -3);
+                        break;
+                    }
+                case "libra (lb)":
+                    {
+                        gram = vstup * 453.59237;
+                        break;
+                    }
+                case "metrický cent (q)":
+                    {
+                        gram = vstup * Math.Pow(10, 5);
+                        break;
+                    }
+                case "unce (oz)":
+                    {
+                        gram = vstup * 28.34952;
+                        break;
+                    }
+
+            }
+
+            /*     
+             *     kilogram (kg)
+             *     gram (g)
+             *     libra (lb)
+             *     tuna (t)
+             *     dekagram  (dag)
+             *     miligram (mg)
+             *     metrický cent (q)
+             *     unce (oz)
+             */
+
+            switch (comVahVystup.Text)
+            {
+
+                case "tuna (t)":
+                    {
+                        vystup = gram / Math.Pow(10, 6);
+                        break;
+                    }
+
+                case "kilogram (kg)":
+                    {
+                        vystup = gram / Math.Pow(10, 3);
+                        break;
+                    }
+
+                case "dekagram (dag)":
+                    {
+                        vystup = gram / Math.Pow(10, 1);
+                        break;
+                    }
+
+                case "gram (g)":
+                    {
+                        vystup = gram;
+                        break;
+                    }
+
+                case "miligram (mg)":
+                    {
+                        vystup = gram * Math.Pow(10, 3);
+                        break;
+                    }
+
+                case "metrický cent (q)":
+                    {
+                        vystup = gram / Math.Pow(10, 5);
+                        break;
+                    }
+
+                case "libra (lb)":
+                    {
+                        vystup = gram / 453.59237;
+                        break;
+                    }
+
+                case "unce (oz)":
+                    {
+                        vystup = gram / 28.34952;
+                        break;
+                    }
+
+            }
+
+            textVystupVah.Text = vystup.ToString("G10");
+        }
+
+        private void numVzda_ValueChanged(object sender, EventArgs e)
+        {
+            /*
+             *       metr (m)
+             *       centimetr (cm)
+             *       palec (in)
+             *       milimetr (mm)
+             *       stopa (ft)
+             *       decimetr (dm)
+             *       kilometr (km)
+             *       míle (mi)
+             *       yard (yd)
+            */
+            double vstup = Convert.ToDouble(numVzda.Value);
+            double vystup = 0;
+            double metr = 0;
+
+            switch (comVzdaVstup.Text)
+            {
+                case "kilometr (km)":
+                    {
+                        metr = vstup * Math.Pow(10, 3);
+                        break;
+                    }
+
+                case "metr (m)":
+                    {
+                        metr = vstup;
+                        break;
+                    }
+
+                case "decimetr (dm)":
+                    {
+                        metr = vstup * Math.Pow(10, -1);
+                        break;
+                    }
+
+                case "centimetr (cm)":
+                    {
+                        metr = vstup * Math.Pow(10, -2);
+                        break;
+                    }
+
+                case "milimetr (mm)":
+                    {
+                        metr = vstup * Math.Pow(10, -3);
+                        break;
+                    }
+
+                case "palec (in)":
+                    {
+                        metr = vstup * 0.0254;
+                        break;
+                    }
+
+                case "stopa (ft)":
+                    {
+                        metr = vstup * 0.3048;
+                        break;
+                    }
+
+                case "míle (mi)":
+                    {
+                        metr = vstup * 1609.344;
+                        break;
+                    }
+
+                case "yard (yd)":
+                    {
+                        metr = vstup * 0.9144;
+                        break;
+                    }
+
+            }
+
+            switch (comVzdaVystup.Text)
+            {
+                case "kilometr (km)":
+                    {
+                        vystup = metr / Math.Pow(10, 3);
+                        break;
+                    }
+
+                case "metr (m)":
+                    {
+                        vystup = metr;
+                        break;
+                    }
+
+                case "decimetr (dm)":
+                    {
+                        vystup = metr / Math.Pow(10, -1);
+                        break;
+                    }
+
+                case "centimetr (cm)":
+                    {
+                        vystup = metr / Math.Pow(10, -2);
+                        break;
+                    }
+
+                case "milimetr (mm)":
+                    {
+                        vystup = metr / Math.Pow(10, -3);
+                        break;
+                    }
+
+                case "palec (in)":
+                    {
+                        vystup = metr / 0.0254;
+                        break;
+                    }
+
+                case "stopa (ft)":
+                    {
+                        vystup = metr / 0.3048;
+                        break;
+                    }
+
+                case "míle (mi)":
+                    {
+                        vystup = metr / 1609.344;
+                        break;
+                    }
+
+                case "yard (yd)":
+                    {
+                        vystup = metr / 0.9144;
+                        break;
+                    }
+
+            }
+
+            textVystupVzda.Text = vystup.ToString("G10");
+        }
+
+        private void ukoncitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
